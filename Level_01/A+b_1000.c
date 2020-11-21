75c74025c3610c80bb923df4560656063fca8a34
SeungHyunLee <esh8904@gmail.com>
2020-11-21 23:55:15 +0900

    20_11_21

--- Level_01/A+b_1000.c
+++ Level_01/A+b_1000.c
@@ -1,10 +1,17 @@
+/*	문제 : 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+	입력 : 첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
+	출력 : 첫째 줄에 A+B를 출력한다.	*/
+
 #include <stdio.h>
 
 int		main(void)
 {
-	int a;
-	int b;
+	int A;
+	int B;
 
-	scanf("%d %d", &a, &b);
-	printf("%d\n", a+b);
+	scanf("%d %d", &A, &B);
+	if (((0 < A) && (B < 10)) && ((0 < A) && (B < 10)))
+		printf("%d\n", A+B);
+	else
+		printf("숫자를 잘못 입력하였습니다.\n프로그램을 종료합니다.");
 }
